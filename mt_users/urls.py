fed5dbbf5918aeee26ea16e54cb2e7bfedfd23e5
chen <cty9388@hotmail.com>
2018-05-19 23:48:33 +0800

    修改了图片部分（手机bug依然存在）

--- mt_users/urls.py
+++ mt_users/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import include,url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.welcome)
+    url(r'^$',views.welcome),
+    url(r'^ceshi/$',views.ceshi)
 ]
--- mt_users/views.py
+++ mt_users/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def welcome(request):
     return render(request,'index.html')
+
+def ceshi(request):
+    return render(request,'ceshi.html')
